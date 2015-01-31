54bb7fecb2fd7fac1abfa5688f3b6e6badc022fe
yatimakov <dairok@rambler.ru>
2015-01-31 17:26:06 +0300

    -

---
+++ dist/server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://92.42.131.153/stats-db'
+  },
+
+  seedDB: true
+};
